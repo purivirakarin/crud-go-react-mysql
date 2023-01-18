33ff4fbd1ee5bdc5f6d5177f7b8b2efc9340a9a6
Puri Virakarin <puri.prv@gmail.com>
2023-01-18 23:24:44 +0800

    successfully did api for register/login/middleware

--- blogbackend/routes/route.go
+++ blogbackend/routes/route.go
@@ -3,8 +3,11 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/purivirakarin/blogbackend/controller"
+	//"github.com/purivirakarin/blogbackend/middleware"
 )
 
 func Setup(app *fiber.App) {
 	app.Post("/api/register", controller.Register)
+	app.Post("/api/login", controller.Login)
+	//app.Use(middleware.IsAuthenticate)
 }
