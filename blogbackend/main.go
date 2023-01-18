505d55d639da57d7bcead0532ee7cf06032c2a94
Puri Virakarin <puri.prv@gmail.com>
2023-01-18 16:39:10 +0800

    add .env and first run go

--- blogbackend/main.go
+++ blogbackend/main.go
@@ -1,2 +1,22 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+	"github.com/purivirakarin/blogbackend/database"
+)
+
+func main() {
+	database.Connect()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env files")
+	}
+	port := os.Getenv("PORT")
+	app := fiber.New()
+	app.Listen(":"+port)
+
+}
