2fa129027a6208d1ebc6352a5af8254e3a8ec4de
Puri Virakarin <puri.prv@gmail.com>
2023-01-19 13:57:56 +0800

    add post controller

--- blogbackend/controller/postController.go
+++ blogbackend/controller/postController.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/purivirakarin/blogbackend/database"
@@ -9,7 +11,7 @@ import (
 )
 
 func CreatePost(c *fiber.Ctx) error {
-	var blogpost models.Blog 
+	var blogpost models.Blog
 	if err := c.BodyParser(&blogpost); err != nil {
 		fmt.Println("Unable to parse body")
 	}
@@ -19,7 +21,25 @@ func CreatePost(c *fiber.Ctx) error {
 			"message": "Invalid payload",
 		})
 	}
-	return c.JSON(fiber.Map {
+	return c.JSON(fiber.Map{
 		"message": "Congratulation!, You post is live!",
+	})
+}
+
+func AllPost(c *fiber.Ctx) error {
+	page, _ := strconv.Atoi(c.Query("page","1"))
+	limit := 5
+	offset := (page - 1) * limit
+	var total int64
+	var getblog models.Blog
+	database.DB.Preload("User").Offset(offset).Limit(limit).Find(&getblog)
+	database.DB.Model(&models.Blog{}).Count(&total)
+	return c.JSON(fiber.Map{
+		"data": getblog,
+		"meta": fiber.Map{
+			"total": total,
+			"page": page,
+			"last_page": math.Ceil(float64(int(total)/limit)),
+		},
 	})
 }
--- blogbackend/routes/route.go
+++ blogbackend/routes/route.go
@@ -3,13 +3,14 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/purivirakarin/blogbackend/controller"
-	//"github.com/purivirakarin/blogbackend/middleware"
+	"github.com/purivirakarin/blogbackend/middleware"
 )
 
 func Setup(app *fiber.App) {
 	app.Post("/api/register", controller.Register)
 	app.Post("/api/login", controller.Login)
 
-	//app.Use(middleware.IsAuthenticate)
+	app.Use(middleware.IsAuthenticate)
 	app.Post("/api/post", controller.CreatePost)
+	app.Get("/api/allpost", controller.AllPost)
 }
